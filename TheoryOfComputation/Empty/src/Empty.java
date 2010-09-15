bb71be8bfdc62b19fc8c7d6b35315c150a8d9666
Skaman Samuel C. Tyler <skamansam@gmail.com>
2010-09-14 22:20:21 -0400

    First commit for Empty.java

---
+++ TheoryOfComputation/Empty/src/Empty.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author sam
+ *
+ */
+public class Empty {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
